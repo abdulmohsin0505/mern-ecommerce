b0dbf7e4923c08a41ff33f4cc74f717d3a72a7ae
abdul mohsin <mohsin050595@gmail.com>
2024-02-08 16:50:33 +0530

    updated

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const Connection = async () => {
-    const url = 'mongodb+srv://mohsin5595:bnox3MYHpP73jDl0@cluster0.d1hvaqi.mongodb.net/?retryWrites=true&w=majority'
-    try {
-        await mongoose.connect(url);
-        console.log('Database Connected Succesfully');
-    } catch(error) {
-        console.log('Error: ', error.message);
-    }
+  // const url = 'mongodb+srv://mohsin5595:bnox3MYHpP73jDl0@cluster0.d1hvaqi.mongodb.net/?retryWrites=true&w=majority'
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Database Connected Succesfully");
+  } catch (error) {
+    console.log("Error: ", error.message);
+  }
 };
 export default Connection;
