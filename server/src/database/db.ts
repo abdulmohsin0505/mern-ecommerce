2acfec0bf5a7052659155b372970330eff7c505f
abdul mohsin <mohsin050595@gmail.com>
2024-02-08 17:06:05 +0530

    updated

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const Connection = async () => {
-  // const url = 'mongodb+srv://mohsin5595:bnox3MYHpP73jDl0@cluster0.d1hvaqi.mongodb.net/?retryWrites=true&w=majority'
   try {
     await mongoose.connect(process.env.MONGO_URL);
     console.log("Database Connected Succesfully");
