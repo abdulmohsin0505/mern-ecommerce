0c7cf7574381c3e78940ce5ee08ae449102a563c
abdul mohsin <mohsin050595@gmail.com>
2024-02-09 21:40:38 +0530

    updated

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, response } from "express";
+import { Request, Response } from "express";
 import User from "../models/userSchema";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
